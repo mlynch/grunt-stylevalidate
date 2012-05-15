860308ea6863aa0b4434944d01d99ecf9a2817ae
Max Lynch <ihasmax@gmail.com>
2012-05-15 13:16:38 -0500

    Did some linting

--- tasks/stylevalidate.js
+++ tasks/stylevalidate.js
@@ -12,7 +12,7 @@ var file = require('file');
 var diff = require('diff');
 var codepainter = require('codepainter');
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
 	// Please see the grunt documentation for more information regarding task and
 	// helper creation: https://github.com/cowboy/grunt/blob/master/docs/toc.md
@@ -22,51 +22,53 @@ module.exports = function(grunt) {
 	// ==========================================================================
 
 	function runCodePainter(directory, style, callback) {
-		file.walk(directory, function(unused, dirPath, dirs, files) {
-			for(var i = 0; i < files.length; i++) {
-				var file = files[i];
+		file.walk(directory, function (unused, dirPath, dirs, files) {
+			var i = 0, 
+				file, input;
 
-				var input = fs.createReadStream(file);
+			for (i = 0; i < files.length; i++) {
+				file = files[i];
+
+				input = fs.createReadStream(file);
 				input.pause();
 
 				grunt.utils.spawn({
 					cmd: '/Users/max/Projects/codepainter/bin/codepaint',
 					args: ['--style', style, '-i', file]
-					}, function(error, output) {
+				}, function (error, output) {
+					fs.readFile(files[0], 'utf-8', function (err, data) {
+						if (err) {
+							console.error('Unable to open source file: %s', err);
+							process.exit(1);
+						}
 
-						fs.readFile(files[0], 'utf-8', function(err, data) {
-							if(err) {
-								console.error('Unable to open source file: %s', err);
-								process.exit(1);
-							}
-
-							callback(files[0], data, output.toString());
-						});
+						callback(files[0], data, output.toString());
+					});
 				});
 			}
 		});
 	}
 
-	grunt.registerMultiTask('styleformat', 'Validate Javascript style against a style definition', function() {
+	grunt.registerMultiTask('styleformat', 'Validate Javascript style against a style definition', function () {
 		var done = this.async(), 
 			dirs = grunt.file.expand(this.file.src);
-		grunt.helper('styleformat', dirs, function(error, result) {
+		grunt.helper('styleformat', dirs, function (error, result) {
 			console.log(error, result);
 		});
 	});
 	
-	grunt.registerHelper('styleformat', function(dirs, style, done) {
+	grunt.registerHelper('styleformat', function (dirs, style, done) {
 		var result = [];
 
-		async.forEach(dirs, function(dir, callback) {
-			runCodePainter(dir, style, function(filename, sourceData, outputData) {
+		async.forEach(dirs, function (dir, callback) {
+			runCodePainter(dir, style, function (filename, sourceData, outputData) {
 				result.push({
 					file: filename,
 					result: 'success',
 					output: outputData
 				});
 				callback();
-			}, function(err) {
+			}, function (err) {
 				result.push({
 					file: filename,
 					result: 'error',
@@ -74,7 +76,7 @@ module.exports = function(grunt) {
 				});
 				callback();
 			});
-		}, function(err) {
+		}, function (err) {
 			done(null, result);
 		});
 	});
@@ -91,20 +93,20 @@ module.exports = function(grunt) {
 	});
 	*/
 	
-	grunt.registerMultiTask('stylevalidate', 'Format Javascript code into a desired style', function() {
+	grunt.registerMultiTask('stylevalidate', 'Format Javascript code into a desired style', function () {
 		var done = this.async(), 
 			files = grunt.file.expand(this.file.src);
 
-		grunt.helper('stylevalidate', files, function(error, result) {
+		grunt.helper('stylevalidate', files, function (error, result) {
 			console.log(error, result);
 		});
 	});
 
-	grunt.registerHelper('stylevalidate', function(dirs, style, done) {
+	grunt.registerHelper('stylevalidate', function (dirs, style, done) {
 		var result = [];
 		//for(var i = 0; i < files.length; i++) {
-		async.forEach(dirs, function(dir, callback) {
-			runCodePainter(dir, style, function(filename, sourceData, outputData) {
+		async.forEach(dirs, function (dir, callback) {
+			runCodePainter(dir, style, function (filename, sourceData, outputData) {
 				var patch = diff.createPatch(filename, sourceData, outputData, "Old Header", "New Header");
 				var diffed = diff.diffLines(sourceData, outputData, "Old Header", "New Header");
 
@@ -120,7 +122,7 @@ module.exports = function(grunt) {
 					result: 'success'
 				});
 				callback();
-			}, function(err) {
+			}, function (err) {
 				result.push({
 					file: filename,
 					result: 'error',
@@ -128,7 +130,7 @@ module.exports = function(grunt) {
 				});
 				callback();
 			});
-		}, function(err) {
+		}, function (err) {
 			done(null, result);
 		});
 
